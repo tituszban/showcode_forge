da177dbf4c8b33ad757f35a503b466b7710a0812
Titusz Ban <tituszban@gmail.com>
2021-12-10 13:58:29 +0000

    Support markdown question files

---
+++ showcode_forge/compilers/question_parser.py
@@ -0,0 +1,31 @@
+import os
+import markdown
+
+
+markdown_html_template = """
+<!DOCTYPE html>
+<html><head></head><body>
+{0}
+</body></html>
+""".lstrip()
+
+
+def load_html(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+def load_markdown(filename):
+    with open(filename) as f:
+        html = markdown.markdown(f.read())
+    return markdown_html_template.format(html)
+
+
+def parse_question(path):
+    _, ext = os.path.splitext(path)
+
+    if ext == ".md":
+        return load_markdown(path)
+    if ext == ".html":
+        return load_html(path)
+    raise Exception(f"Unsupported file type {ext}")
